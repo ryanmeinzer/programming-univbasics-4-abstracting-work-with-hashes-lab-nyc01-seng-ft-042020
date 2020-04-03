413333c83942e781beb003b44c0ad1a11add0bab
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-03 20:56:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,5 @@
-def my_hash_creator(key, value)
+def my_hash_creator(name:, "Ryan")
   # return a hash that includes the key and value parameters passed into this method
-  {key: "value"}
 end
 
 def read_from_hash(hash, key)
